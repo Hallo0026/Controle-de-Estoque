1112fb5867f182b55920a55b5449dd7d9f438e5e
Hallo0026 <vinicius_santossouza@outlook.com>
2024-05-03 21:59:15 -0300

    .

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,9 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+
+                    <header-component></header-component>
+
                 </div>
             </div>
         </div>
